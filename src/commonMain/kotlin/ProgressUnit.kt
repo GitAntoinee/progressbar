253210fe066e029b8917724f15cd12c4e1a0ad6e
GitAntoinee <apottime@gmail.com>
2020-04-12 12:09:36 +0200

    Add common units

--- src/commonMain/kotlin/ProgressUnit.kt
+++ src/commonMain/kotlin/ProgressUnit.kt
@@ -3,5 +3,10 @@ package fr.pottime.progressbar
 data class ProgressUnit(val name: String, val size: Double) {
     companion object {
         val DEFAULT: ProgressUnit = ProgressUnit("", 1.0)
+
+        val KB_TO_MB: ProgressUnit = ProgressUnit("MB", 1000.0)
+        val KiB_TO_MiB: ProgressUnit = ProgressUnit("MiB", 1024.0)
+        val MB_TO_KB: ProgressUnit = ProgressUnit("KB", 0.1000)
+        val MiB_TO_KiB: ProgressUnit = ProgressUnit("KB", 0.1024)
     }
 }
