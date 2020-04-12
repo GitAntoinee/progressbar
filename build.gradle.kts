9ef92faf01c3507e6c02814abf9624713dd29382
GitAntoinee <apottime@gmail.com>
2020-04-12 15:10:22 +0200

    Set version to 1.0 and add bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("multiplatform") version "1.4-M1"
     id("com.github.johnrengelman.shadow") version "5.2.0" // Fat jar of sample
+
+    // Publication related
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 group = "fr.pottime.progressbar"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
+    maven("https://dl.bintray.com/pottime/maven")
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
     maven("https://dl.bintray.com/kotlin/kotlinx")
     jcenter()
@@ -17,7 +22,11 @@ repositories {
 
 kotlin {
     jvm()
-    jvm("sampleJvm")
+    jvm("sampleJvm") {
+        this.attributes {
+            this.attribute(Attribute.of("fr.pottime.progressbar.sample", String::class.java), "jvm")
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -50,7 +59,34 @@ kotlin {
     }
 }
 
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+
+    pkg.apply {
+        userOrg = "pottime"
+        repo = "maven"
+        name = "progressbar"
+
+        setLicenses("MIT")
+        vcsUrl = "https://github.com/GitAntoinee/progressbar.git"
+        websiteUrl = "https://github.com/GitAntoinee/progressbar"
+        issueTrackerUrl = "https://github.com/GitAntoinee/progressbar/issues"
+
+        version.apply {
+            name = project.version.toString()
+            vcsTag = project.version.toString()
+        }
+    }
+}
+
 tasks {
+    bintrayUpload.get().doFirst {
+        this as com.jfrog.bintray.gradle.tasks.BintrayUploadTask
+
+        setPublications(*publishing.publications.map { it.name }.filter { it != "kotlinMultiplatform " }.toTypedArray())
+    }
+
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
