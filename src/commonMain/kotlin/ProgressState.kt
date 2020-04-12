f844fb7e0e6996997f814780a0488e44a15d42be
GitAntoinee <apottime@gmail.com>
2020-04-12 17:51:26 +0200

    Remove unnecessary field

--- src/commonMain/kotlin/ProgressState.kt
+++ src/commonMain/kotlin/ProgressState.kt
@@ -15,11 +15,3 @@ val ProgressState.progress: Double
     }
 
 val ProgressState.percentage: Double get() = progress * 100
-
-val ProgressState.percentageLength: Int
-    get() = when (percentage) {
-        100.0 -> 7 // 100.00%
-        in 10.0..100.0 -> 6 // 99.99%
-        in 0.0..10.0 -> 5 // 9.99%
-        else -> error("Percentage out of range")
-    }
